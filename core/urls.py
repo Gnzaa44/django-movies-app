7f39f6a67bf586b36fee526e752f76e741eaaa9a
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-11-04 18:32:14 -0300

    stable version with local development working fine.

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', include('movies.urls')),
 ]
 
+# CRÍTICO: Agregar ambas líneas para DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
